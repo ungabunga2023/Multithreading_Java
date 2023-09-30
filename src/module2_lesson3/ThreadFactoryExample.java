b58bafe15320ec6a13b13481ee7e70494e241f71
Arina Umushkina <umushkina.arina@yandex.ru>
2023-09-30 21:47:28 +0300

    module 2 lesson 3, ExecutorExample ScheduledFixedDelayExample ScheduledThreadPoolExample

---
+++ src/module2_lesson3/ThreadFactoryExample.java
@@ -0,0 +1,31 @@
+package module2_lesson3;
+
+import java.util.concurrent.ThreadFactory;
+
+public class ThreadFactoryExample {
+
+    public static void main(String[] args) {
+        Runnable task = () -> System.out.println("Я родился! " + Thread.currentThread().getName());
+        CustomThreadFactory customThreadFactory = new CustomThreadFactory("Фабричный поток");
+
+        customThreadFactory.newThread(task).start();
+        customThreadFactory.newThread(task).start();
+        customThreadFactory.newThread(task).start();
+    }
+}
+
+class CustomThreadFactory implements ThreadFactory {
+
+    private String name;
+
+    public CustomThreadFactory(String name) {
+        this.name = name;
+    }
+
+    public Thread newThread(Runnable r) {
+        Thread thread = new Thread(r);
+        thread.setName(name);
+        thread.setPriority(7);
+        return thread;
+    }
+}
---
+++ src/module2_lesson3/ThreadFactoryExecutorExample.java
@@ -0,0 +1,36 @@
+package module2_lesson3;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+
+public class ThreadFactoryExecutorExample {
+
+    public static void main(String[] args) {
+        Runnable task = () -> System.out.println("Я родился! " + Thread.currentThread().getName()); //полезная нагрузка потока
+
+        CustomExecutorThreadFactory customThreadFactory = new CustomExecutorThreadFactory("Фабричный поток"); //шаблон потока
+        ExecutorService fixedPool = Executors.newSingleThreadExecutor(customThreadFactory); // создастся один поток
+
+        for (int i = 0; i < 10; i++) {
+            fixedPool.submit(task);
+        }
+        fixedPool.shutdown();
+    }
+}
+
+class CustomExecutorThreadFactory implements ThreadFactory {
+
+    private String name;
+
+    public CustomExecutorThreadFactory(String name) {
+        this.name = name;
+    }
+
+    public Thread newThread(Runnable r) {
+        Thread thread = new Thread(r);
+        thread.setName(name);
+        return thread;
+    }
+}
