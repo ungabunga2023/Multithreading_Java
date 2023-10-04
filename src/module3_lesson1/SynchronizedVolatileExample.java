a62e20daa47bc5492ab0e55fce637140d86c1cb3
Arina Umushkina <umushkina.arina@yandex.ru>
2023-10-04 19:49:15 +0300

    module 3 lesson 1, приимеры с ошибками

---
+++ src/module3_lesson1/SynchronizedVolatileExample.java
@@ -0,0 +1,42 @@
+package module3_lesson1;
+
+public class SynchronizedVolatileExample {
+    //todo код содержит ошибку
+    public static void main(String[] args) throws InterruptedException {
+        VolatileIncrementer incrementer = new VolatileIncrementer();
+        for (int i = 0; i < 200; i++) { //создали 200 потоков, каждый увеличил на 1000
+            MyThread ct = new MyThread(incrementer);
+            new Thread(ct).start();
+        }
+        Thread.sleep(3000);
+        System.out.println("Counter:" + incrementer.getAmount()); //должно быть 200_000
+    }
+}
+
+class VolatileIncrementer {
+    //неправильно
+    private volatile long amount = 0L;
+
+    public void increaseAmount() {
+        amount++;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+}
+
+class MyThread implements Runnable {
+    private final VolatileIncrementer incrementer;
+
+    public MyThread(VolatileIncrementer incrementer) {
+        this.incrementer = incrementer;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            incrementer.increaseAmount();
+        }
+    }
+}
